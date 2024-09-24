b17594ebb115a6ac4a5c324a11ce29dd75fcbe29
Mohammed Aburidi <48891624+Maburidi@users.noreply.github.com>
2024-09-24 11:55:00 -0700

    Update IFR_bl_Redinger_Lake_Min_Flow.py

--- sierra/models/upper_san_joaquin/_parameters/IFR_bl_Redinger_Lake_Min_Flow.py
+++ sierra/models/upper_san_joaquin/_parameters/IFR_bl_Redinger_Lake_Min_Flow.py
@@ -1,6 +1,6 @@
-from cen_sierra_pywr.sierra.base_parameters import MinFlowParameter
+from cen_sierra_pywr_new.sierra.base_parameters import MinFlowParameter
 
-from cen_sierra_pywr.sierra.utilities.converter import convert
+from cen_sierra_pywr_new.sierra.utilities.converter import convert
 
 
 class IFR_bl_Redinger_Lake_Min_Flow(MinFlowParameter):
