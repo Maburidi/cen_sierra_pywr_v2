e3125833ea005dd85aafd499e229c245f45bf328
Mohammed Aburidi <48891624+Maburidi@users.noreply.github.com>
2024-09-24 11:49:26 -0700

    Update FlowRangeParameter.py

--- sierra/base_parameters/FlowRangeParameter.py
+++ sierra/base_parameters/FlowRangeParameter.py
@@ -1,5 +1,5 @@
-from cen_sierra_pywr.sierra.base_parameters import IFRParameter
-from cen_sierra_pywr.sierra.utilities.converter import convert
+from cen_sierra_pywr_new.sierra.base_parameters import IFRParameter
+from cen_sierra_pywr_new.sierra.utilities.converter import convert
 
 
 class FlowRangeParameter(IFRParameter):
