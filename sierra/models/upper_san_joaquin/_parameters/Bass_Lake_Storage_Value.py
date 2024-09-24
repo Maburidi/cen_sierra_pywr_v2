6cc7bf90fc7223c9fa232b23a7a8f0ba7321ca52
Mohammed Aburidi <48891624+Maburidi@users.noreply.github.com>
2024-09-24 11:50:49 -0700

    Update Bass_Lake_Storage_Value.py

--- sierra/models/upper_san_joaquin/_parameters/Bass_Lake_Storage_Value.py
+++ sierra/models/upper_san_joaquin/_parameters/Bass_Lake_Storage_Value.py
@@ -1,4 +1,4 @@
-from cen_sierra_pywr.sierra.base_parameters import BaseParameter
+from cen_sierra_pywr_new.sierra.base_parameters import BaseParameter
 
 
 class Bass_Lake_Storage_Value(BaseParameter):
