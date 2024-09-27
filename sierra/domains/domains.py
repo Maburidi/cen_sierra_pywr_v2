495b7ca003f58df76140e5350c962860b71d2d27
Mohammed Aburidi <48891624+Maburidi@users.noreply.github.com>
2024-09-26 19:05:33 -0700

    Update domains.py

--- sierra/domains/domains.py
+++ sierra/domains/domains.py
@@ -106,6 +106,7 @@ class InstreamFlowRequirement(PiecewiseLink):
         # create keyword arguments for PiecewiseLink
         kwargs['cost'] = kwargs.pop('cost', [0.0, 0.0, 0.0])
         kwargs['max_flow'] = kwargs.pop('max_flow', [0.0, 0.0, 0.0])
+        kwargs['nsteps'] = len(kwargs['cost'])
         #kwargs['max_flow'].append(None)
         try:
             assert (len(kwargs['cost']) == len(kwargs['max_flow']))
