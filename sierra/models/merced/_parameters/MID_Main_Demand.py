037d52df9683dfe0a02f7464eae0b753ba0df3e3
Mohammed Aburidi <mohammedaburidi@Mohammeds-MacBook-Pro.local>
2024-10-07 15:16:24 -0700

    “data”

--- sierra/models/merced/_parameters/MID_Main_Demand.py
+++ sierra/models/merced/_parameters/MID_Main_Demand.py
@@ -1,16 +1,38 @@
-from cen_sierra_pywr_new.sierra.base_parameters import BaseParameter
+from parameters import WaterLPParameter
 
-from cen_sierra_pywr_new.sierra.utilities.converter import convert
+from utilities.converter import convert
 
 
-class MID_Main_Demand(BaseParameter):
+class MID_Main_Demand(WaterLPParameter):
     """"""
-    MID_bias_factor = 1.14
+
+    reductions = [0, 0]
 
     def _value(self, timestep, scenario_index):
-        WYT = self.model.tables['WYT for IFR Below Exchequer'][self.operational_water_year]
+        m3_to_cfs = 35.31
+        type_value = self.model.tables['WYT for IFR Below Exchequer'][timestep.year]
         ts = "{}/{}/1900".format(timestep.month, timestep.day)
-        demand_cms = self.model.tables["MID Main Diversions"].at[ts, WYT]*self.MID_bias_factor / 35.31
+
+        if type_value == 1:
+            year_type = "Critical"
+        elif type_value == 2:
+            year_type = "Dry"
+        elif type_value == 3:
+            year_type = "Below"
+        elif type_value == 4:
+            year_type = "Above"
+        else:
+            year_type = "Wet"
+
+        demand_cms = self.model.tables["MID Main Diversions"].at[ts, year_type] / 35.31
+
+        idx = scenario_index.indices[1]
+        reduction = 0.0
+        if idx == 1:
+            ifr_param = self.model.parameters["IFR bl Crocker-Huffman Dam/Requirement"]
+            reduction = ifr_param.swrcb_levels[scenario_index.indices[0]]
+
+        demand_cms *= (1 - reduction)
         return demand_cms
 
     def value(self, timestep, scenario_index):
@@ -22,3 +44,4 @@ class MID_Main_Demand(BaseParameter):
 
 
 MID_Main_Demand.register()
+print(" [*] MID_Main_Demand successfully registered")
--- sierra/models/merced/_parameters/MID_Northside_Demand.py
+++ sierra/models/merced/_parameters/MID_Northside_Demand.py
@@ -1,21 +1,39 @@
-from cen_sierra_pywr_new.sierra.base_parameters import BaseParameter
+from parameters import WaterLPParameter
 
-from cen_sierra_pywr_new.sierra.utilities.converter import convert
+from utilities.converter import convert
 
 
-class MID_Northside_Demand(BaseParameter):
+class MID_Northside_Demand(WaterLPParameter):
     """"""
 
     reductions = [0, 0]
-    
-    MID_North_bias_factor = 1.06
 
     def _value(self, timestep, scenario_index):
 
-        WYT = self.model.tables['WYT for IFR Below Exchequer'][self.operational_water_year]
+        type_value = self.model.tables['WYT for IFR Below Exchequer'][timestep.year]
         ts = "{}/{}/1900".format(timestep.month, timestep.day)
-        demand_cms = self.model.tables["MID Northside Diversions"].at[ts, WYT]*self.MID_North_bias_factor / 35.31
 
+        if type_value == 1:
+            year_type = "Critical"
+        elif type_value == 2:
+            year_type = "Dry"
+        elif type_value == 3:
+            year_type = "Below"
+        elif type_value == 4:
+            year_type = "Above"
+        else:
+            year_type = "Wet"
+
+        demand_cms = self.model.tables["MID Northside Diversions"].at[ts, year_type] / 35.31
+
+        idx = scenario_index.indices[1]
+        if timestep.month == 1 and timestep.day == 1:
+            if idx == 1:
+                ifr_param = self.model.parameters["IFR bl Crocker-Huffman Dam/Requirement"]
+                reduction = ifr_param.swrcb_levels[scenario_index.indices[0]]
+                self.reductions[1] = reduction
+
+        demand_cms *= (1 - self.reductions[idx])
         return demand_cms
 
     def value(self, timestep, scenario_index):
@@ -27,3 +45,4 @@ class MID_Northside_Demand(BaseParameter):
 
 
 MID_Northside_Demand.register()
+print(" [*] MID_Northside_Demand successfully registered")
